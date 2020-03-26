dcb622eac2298a45df35d37d1c6a644f17feddff
Charles Catta <charles.catta@gmail.com>
2020-03-26 13:07:02 -0400

    Add React Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-import NavBar from '@components/nav-bar/index';
+import Homepage from './pages/Home';
 import GlobalStyles from './global-styles';
 
 const App = () => (
-    <div>
-      <GlobalStyles />
-
-    </div>
+  <>
+    <GlobalStyles />
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route path="*">
+          <h1>404 NOT FOUND</h1>
+        </Route>
+      </Switch>
+    </Router>
+  </>
 );
 
 ReactDOM.render(<App />, document.querySelector('#root'));
