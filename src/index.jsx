e0dcb3f27a4248f0ec5dff9952c40867d6565372
Charles Catta <charles.catta@gmail.com>
2020-03-26 12:28:46 -0400

    Remove non-starter-kit content

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import sushivid from '@img/videos/sushis.mp4';
-import logo from '@img/logo-sushi';
 import NavBar from '@components/nav-bar/index';
 import GlobalStyles from './global-styles';
 
 const App = () => (
     <div>
-        <GlobalStyles />
-        <NavBar />
-        <div>
-            <img style={{ position: 'fixed', maxWidth: '300px' }} src={logo} />
-            <video style={{ position: 'fixed', width: '100vw', zIndex: 0 }} width="100%" src={sushivid} autoPlay muted playsInline loop />
-        </div>
+      <GlobalStyles />
+
     </div>
 );
 
